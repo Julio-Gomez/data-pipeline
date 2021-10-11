0d0906f16937a3cf3fdb50503455cf2a674e575f
julio <jgcesar44@gmail.com>
2021-10-11 01:10:22 -0500

    Agregando modulos para la base de datos.

---
+++ py/bd.py
@@ -0,0 +1,74 @@
+from . import config as cnf
+
+import sqlite3
+
+
+def connect():
+    con = sqlite3.connect(cnf.path_bd)
+
+    return con
+
+
+def query_insert_registros(tabla: str, registros: list, columnas: tuple = ()):
+    """
+    :param tabla: nombre de la tabla
+    :param columnas:
+    :param registros:
+    :return: 'INSERT INTO tabla (columna1, columna2,...) VALUES (registro1, registro2, ...), (r2, r,...)'
+    """
+    query_columnas = "(`" + "`, `".join(columnas) + "`)" if columnas else ''
+
+    temporal = []
+
+    for registro in registros:
+        if not registro:
+            continue
+
+        registro_empaquetado = empaquetamiento_de_registro(registro)
+
+        temporal.append(registro_empaquetado)
+
+    query_values = ", ".join(temporal)
+
+    return '''
+        INSERT INTO 
+            {tabla} {columnas} 
+        VALUES
+            {values}
+    '''.format(tabla=tabla, columnas=query_columnas, values=query_values)
+
+
+def empaquetamiento_de_registro(registro: list):
+    regtemp = [x if x is not None else 'NoneReserveRemplace' for x in registro]
+
+    empaquetado = str(tuple(regtemp)) if len(regtemp) > 1 else str(tuple(regtemp))[:-2] + ')'
+
+    return empaquetado.replace("\'NoneReserveRemplace\'", 'null')
+
+
+class BD:
+    def __init__(self):
+        self.con = connect()
+
+    def __del__(self):
+        self.con.close()
+
+    def abrircursor(self):
+        return self.con.cursor()
+
+    def execute(self, sql):
+        cursor = self.abrircursor()
+
+        try:
+            cursor.execute(sql)
+            self.con.commit()
+        except sqlite3.Error as e:
+            print(e)
+            self.con.rollback()
+            raise e
+
+        self.cerrar_cursor(cursor)
+
+    @staticmethod
+    def cerrar_cursor(cursor):
+        cursor.close()
--- py/config.py
+++ py/config.py
@@ -5,8 +5,12 @@ path_parent = os.path.dirname(path_module)
 path_project = os.path.abspath(os.path.join(path_parent, '..'))
 
 path_files = os.path.join(path_project, 'files')
+path_bd = os.path.join(path_project, 'bd', 'data_pipeline.db')
+
 path_limite_alcaldias = os.path.join(path_files, 'limite-de-las-alcaldias.csv')
 
 ALCALDIA_NO_ENCONTRADO = 'No encontrada'
 CHUNKSIZE_DEFAULT_RECORDS_UBICACION_METROBUS = 100
 URL_ROOT_DATOS_CDMX = 'https://datos.cdmx.gob.mx'
+
+TABLA_UBICMETRO = "ubicmetro"
